4450bc9aea7683a03df4a8b4914222db49067c4b
wanqian99 <gaowanqian.99@gmail.com>
2023-04-24 18:55:40 +0800

    Build platform selector

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
 
 function App() {
 	const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
@@ -39,6 +40,7 @@ function App() {
 				</Show>
 				{/* MAIN CONTENT */}
 				<GridItem area={"main"}>
+					<PlatformSelector />
 					{/* gets selected genre state to display games */}
 					{/* displays all games if null */}
 					<GameGrid selectedGenre={selectedGenre} />
