c00ff6f9b9e33df4ec240a5596e6ef0b0881a191
wanqian99 <gaowanqian.99@gmail.com>
2023-05-05 19:26:00 +0800

    refactor fetch genres and platforms - react query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -20,7 +20,7 @@ const PlatformSelector = ({ selectedPlatform, onSelectPlatform }: Props) => {
 					{selectedPlatform?.name || "Platforms"}
 				</MenuButton>
 				<MenuList>
-					{data.map((platform) => (
+					{data?.results.map((platform) => (
 						<MenuItem
 							key={platform.id}
 							onClick={() => onSelectPlatform(platform)}
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,8 @@
 // import useData from "./useData";
-import genres from "../data/genres"
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
     id: number;
@@ -8,6 +11,14 @@ export interface Genre {
 }
 
 // const useGenres = () => useData<Genre>('/genres');
-const useGenres = () => ({data: genres, isLoading: false, error: null})
+// const useGenres = () => ({data: genres, isLoading: false, error: null})
+const useGenres = () => useQuery({
+    queryKey: ['genres'],
+    queryFn: () => 
+        apiClient
+        .get<FetchResponse<Genre>>('/genres').then(res => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24hrs (60mins, 60secs, 60milliseconds)
+    initialData: {count: genres.length, results: genres}, 
+})
 
 export default useGenres;
