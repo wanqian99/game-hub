c2fab8bc46c86a264f6f2041c2b57ca2ffa8f055
wanqian99 <gaowanqian.99@gmail.com>
2023-05-05 21:07:01 +0800

    Refactoring - fetch games with react query

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,41 +1,41 @@
-import { AxiosRequestConfig, CanceledError } from "axios";
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
+// import { AxiosRequestConfig, CanceledError } from "axios";
+// import { useEffect, useState } from "react";
+// import apiClient from "../services/api-client";
 
-export interface FetchResponse<T> {
-    count: number;
-    results: T[];
-}
+// export interface FetchResponse<T> {
+//     count: number;
+//     results: T[];
+// }
 
-// <T> generic type function
-const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
-    const [data, setData] = useState<T[]>([]);
-	const [error, setError] = useState("");
-    const [isLoading, setLoading] = useState(false);
+// // <T> generic type function
+// const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
+//     const [data, setData] = useState<T[]>([]);
+// 	const [error, setError] = useState("");
+//     const [isLoading, setLoading] = useState(false);
 
-	useEffect(() => {
-        const controller = new AbortController();
+// 	useEffect(() => {
+//         const controller = new AbortController();
 
-        setLoading(true);
-		apiClient
-			.get<FetchResponse<T>>(endpoint, { signal: controller.signal, ...requestConfig })
-			.then((res) => {
-                setData(res.data.results);
-                setLoading(false);
-            })
-			.catch((err) => {
-                if(err instanceof CanceledError) return;
-                setError(err.message);
-                setLoading(false);
-            });
+//         setLoading(true);
+// 		apiClient
+// 			.get<FetchResponse<T>>(endpoint, { signal: controller.signal, ...requestConfig })
+// 			.then((res) => {
+//                 setData(res.data.results);
+//                 setLoading(false);
+//             })
+// 			.catch((err) => {
+//                 if(err instanceof CanceledError) return;
+//                 setError(err.message);
+//                 setLoading(false);
+//             });
 
-            return () => controller.abort();
+//             return () => controller.abort();
         
-        // since deps is optional, it can be undefined, cant spread undefined.
-        // so if dependencies is true, spread it. else empty array
-	}, deps ? [...deps] : []);
+//         // since deps is optional, it can be undefined, cant spread undefined.
+//         // so if dependencies is true, spread it. else empty array
+// 	}, deps ? [...deps] : []);
 
-    return { data, error, isLoading };
-};
+//     return { data, error, isLoading };
+// };
 
-export default useData;
+// export default useData;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
+import { FetchResponse } from "../services/api-client";
+import apiClient from "../services/api-client";
 
 export interface Platform {
     id: number;
@@ -18,16 +20,28 @@ export interface Game {
 
 // selected genre can be either Genre type, or null (all games)
 const useGames = (gameQuery: GameQuery) => 
-    // api path link, axios request config, dependencies
-    useData<Game>(
-        '/games', 
-        {params: {
-                genres: gameQuery.genre?.id, 
-                platforms: gameQuery.platform?.id,
-                ordering: gameQuery.sortOrder,
-                search: gameQuery.searchText
-            }
-        }, 
-        [gameQuery]);
+    useQuery<FetchResponse<Game>, Error>({
+        queryKey: ['games', gameQuery],
+        queryFn: () => apiClient.get<FetchResponse<Game>>('/games', {
+                params: {
+                    genres: gameQuery.genre?.id, 
+                    parent_platforms: gameQuery.platform?.id,
+                    ordering: gameQuery.sortOrder,
+                    search: gameQuery.searchText
+                }
+        })
+        .then(res => res.data)
+    })
+    // // api path link, axios request config, dependencies
+    // useData<Game>(
+    //     '/games', 
+    //     {params: {
+    //             genres: gameQuery.genre?.id, 
+    //             platforms: gameQuery.platform?.id,
+    //             ordering: gameQuery.sortOrder,
+    //             search: gameQuery.searchText
+    //         }
+    //     }, 
+    //     [gameQuery]);
 
 export default useGames;
