0b91fd1653b92410015941872097d6100835f08b
wanqian99 <gaowanqian.99@gmail.com>
2023-05-06 12:29:53 +0800

    implement infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,11 @@
-import { Box, Button, SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { GameQuery } from "../App";
 import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
 	gameQuery: GameQuery;
@@ -23,9 +24,30 @@ const GameGrid = ({ gameQuery }: Props) => {
 
 	if (error) return <Text>{error.message}</Text>;
 
+	// total number of items fetched so far
+	// reduce: combine the number of games on each page into a number
+	// total: accumulator (variable that contaisn the total)
+	// page: element in the pages array
+	// count number of of games on that page, and add it to total
+	// 0: initial value for the total
+	const fetchGamesCount =
+		data?.pages.reduce((total, page) => total + page.results.length, 0) ||
+		0;
+
 	return (
-		<Box padding={"10px"}>
-			<SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
+		// <Box padding={"10px"}>
+		<InfiniteScroll
+			dataLength={fetchGamesCount}
+			// !! to convert to boolean value. so if undefined, it will be boolean false
+			hasMore={!!hasNextPage}
+			next={() => fetchNextPage()}
+			loader={<Spinner />}
+		>
+			<SimpleGrid
+				columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+				spacing={6}
+				padding={"10px"}
+			>
 				{isLoading &&
 					skeletons.map((skeleton) => (
 						<GameCardContainer key={skeleton}>
@@ -42,12 +64,13 @@ const GameGrid = ({ gameQuery }: Props) => {
 					</React.Fragment>
 				))}
 			</SimpleGrid>
-			{hasNextPage && (
-				<Button onClick={() => fetchNextPage()} marginY={5}>
-					{isFetchingNextPage ? "Loading..." : "Load More"}
-				</Button>
-			)}
-		</Box>
+		</InfiniteScroll>
+		// {/* {hasNextPage && (
+		// 	<Button onClick={() => fetchNextPage()} marginY={5}>
+		// 		{isFetchingNextPage ? "Loading..." : "Load More"}
+		// 	</Button>
+		// )} */}
+		// </Box>
 	);
 };
 
