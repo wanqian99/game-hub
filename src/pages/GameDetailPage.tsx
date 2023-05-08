4be03f005321808c285a271fa2e2c6d46460bc1e
wanqian99 <gaowanqian.99@gmail.com>
2023-05-08 15:12:28 +0800

    build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,10 +1,9 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
-import DefinitionItem from "../components/DefinitionItem";
-import CriticScore from "../components/CriticScore";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 
 const GameDetailPage = () => {
 	const { slug } = useParams();
@@ -18,6 +17,7 @@ const GameDetailPage = () => {
 			<Heading>{game.name}</Heading>
 			<ExpandableText>{game.description_raw}</ExpandableText>
 			<GameAttributes game={game} />
+			<GameTrailer gameId={game.id} />
 		</>
 	);
 };
